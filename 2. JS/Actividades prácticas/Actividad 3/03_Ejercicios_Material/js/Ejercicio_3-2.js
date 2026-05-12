e5da3b3f22eeccf881294f620d78b0f21d8a74d2
anasansegundo98 <anasansegundo98@gmail.com>
2026-05-12 09:53:20 +0200

    Ejercicio 4.1-4.2

--- 2. JS/Actividades prácticas/Actividad 3/03_Ejercicios_Material/js/Ejercicio_3-2.js
+++ 2. JS/Actividades prácticas/Actividad 3/03_Ejercicios_Material/js/Ejercicio_3-2.js
@@ -1,5 +1,18 @@
 // Escribir en este fichero las funciones factorial() y convertir() pedidas en el enunciado 
+function factorial(num1) {
+    num1 = parseInt(num1);
+    let resultado = 1;
+    for (i = num1; i >= 1; i--) {
+        resultado = resultado * i;
+    }
+    return resultado;
+}
 
+function convertir(idCentigrados) {
+    idCentigrados = parseInt(idCentigrados);
+    let conversion = (idCentigrados * 9 / 5) + 32;
+    return conversion;
+}
 
 
 
--- 2. JS/Actividades prácticas/Actividad 4/04_Ejercicios_Material/js/Ejercicio_4-1.js
+++ 2. JS/Actividades prácticas/Actividad 4/04_Ejercicios_Material/js/Ejercicio_4-1.js
@@ -4,18 +4,28 @@
 // La función escribe() tiene un parámetro de entrada con una cadena de texto con el código
 // HTML que se quiere mostrar dentro de la capa con ID "cuadro_texto". Esta función el alumno
 // tiene que usarla pero no es necesario modificarla
-function escribe(texto){
-	
-	document.getElementById("cuadro_texto").innerHTML=texto;
-	
+function escribe(texto) {
+	document.getElementById("cuadro_texto").innerHTML = texto;
 }
 
-function ponfecha(){
+function ponfecha() {
 
 // Aqui se tiene que poner el código que haga lo que pide el enunciado.
 
+	var d = new Date();
+	var semana = ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"];
+	var dia_semana = d.getDay();
+	var texto_dia_semana = semana[dia_semana];
 
+	var mes = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+	var numero_mes = d.getMonth();
+	var texto_mes = mes[numero_mes];
 
+	var dia_mes = d.getDate();
+	var anyo = d.getFullYear();
+
+	document.write("Hoy es " + texto_dia_semana + ", " + dia_mes + " de " + texto_mes + " de " + anyo);
 
 
 }
+
